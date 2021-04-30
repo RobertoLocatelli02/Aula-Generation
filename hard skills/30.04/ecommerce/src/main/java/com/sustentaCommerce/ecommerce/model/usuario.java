496534b08a75592105afed36d6888680703ff2b1
Roberto Locatelli <rlocatelli02@gmail.com>
2021-04-30 18:46:03 -0300

    adicionando tarefas do dia 30/04

---
+++ hard skills/30.04/ecommerce/src/main/java/com/sustentaCommerce/ecommerce/model/usuario.java
@@ -0,0 +1,75 @@
+package com.sustentaCommerce.ecommerce.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.UniqueElements;
+
+@Entity
+@Table(name = "tb_usuario_vendedor")
+public class usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String nome_completo;
+	
+	@NotNull
+	@UniqueElements
+	@Size(min = 5, max = 100)
+	private String email;
+	
+	@NotNull
+	private String senha;
+	
+	@NotNull
+	private String tipo_pagamento;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome_completo() {
+		return nome_completo;
+	}
+
+	public void setNome_completo(String nome_completo) {
+		this.nome_completo = nome_completo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getTipo_pagamento() {
+		return tipo_pagamento;
+	}
+
+	public void setTipo_pagamento(String tipo_pagamento) {
+		this.tipo_pagamento = tipo_pagamento;
+	}
+}
