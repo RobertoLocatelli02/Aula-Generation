cc4657cef5125d407f777eea79b48afbc6a068cf
Roberto Locatelli <rlocatelli02@gmail.com>
2021-06-04 08:30:53 -0300

    adicionando componente-tema

--- Angular/blogPessoal/blogPessoal/src/app/tema/tema.component.ts
+++ Angular/blogPessoal/blogPessoal/src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Categoria } from '../model/Categoria';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -9,12 +11,31 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private temaService: TemaService) { }
+
+  tema: Categoria = new Categoria
+  listaTemas: Categoria[]
 
   ngOnInit() {
     if(environment.token == '') {
       this.router.navigate(['/entrar'])
     }
+
+    this.findAllTemas()
   }
 
+  findAllTemas() {
+    this.temaService.getAllCategoria().subscribe((resp: Categoria[]) => {
+      this.listaTemas = resp
+    })
+  }
+
+  cadastrar() {
+    this.temaService.postTema(this.tema).subscribe((resp: Categoria) => {
+      this.tema = resp
+      alert('Tema cadastrado com sucesso')
+      this.findAllTemas()
+      this.tema = new Categoria()
+    })
+  }
 }
