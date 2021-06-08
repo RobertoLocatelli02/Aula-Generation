d5a12de2dec1f695fcbd1475586501db1c0d3257
Roberto Locatelli <rlocatelli02@gmail.com>
2021-06-08 12:25:14 -0300

    adicionando métodos post e get em postagem

--- Angular/blogPessoal/blogPessoal/src/app/inicio/inicio.component.ts
+++ Angular/blogPessoal/blogPessoal/src/app/inicio/inicio.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Categoria } from '../model/Categoria';
+import { Postagem } from '../model/Postagem';
+import { Usuario } from '../model/Usuario';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -9,12 +14,48 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class InicioComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  postagem: Postagem = new Postagem()
+
+  categoria : Categoria = new Categoria()
+  listaCategorias: Categoria[]
+  idCategoria: number
+
+  user: Usuario = new Usuario()
+  idUser = environment.id
+
+  constructor(private router: Router, private postagemService: PostagemService, private temaService: TemaService) { }
 
   ngOnInit() {
     if(environment.token == '') {
       this.router.navigate(['/entrar'])
     }
+
+    this.getAllTemas()
   }
 
+  getAllTemas() {
+    this.temaService.getAllCategoria().subscribe((resp: Categoria[]) => {
+      this.listaCategorias = resp
+    })
+  }
+
+  findByIdTema() {
+    this.temaService.getByIdTema(this.idCategoria).subscribe((resp: Categoria) => {
+      this.categoria = resp
+    })
+  }
+
+  publicar() {
+    this.categoria.id = this.idCategoria
+    //this.postagem.categoria = this.categoria
+
+    this.user.id = this.idUser
+    //this.postagem.usuario = this.user
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) => {
+      this.postagem = resp
+      alert("Postagem realizada com sucesso")
+      this.postagem = new Postagem()
+    })
+  }
 }
